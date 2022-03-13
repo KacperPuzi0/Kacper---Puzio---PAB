b82f6c970b19c77fba8528b5cb1bfc098e53c323
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-03-13 02:12:02 +0100

    Calculator

--- Lab1-09.03/index.js
+++ Lab1-09.03/index.js
@@ -3,4 +3,28 @@ var app = express();
 app.get('/', function (req, res) {
     res.render("index")
 });
+app.get("/add/:num1/:num2", function(req,res){
+    var number1 = parseFloat(req.params.num1)
+    var number2 = parseFloat(req.params.num2)
+    var sum = number1 + number2
+    res.render("add", {number:sum})
+});
+app.get("/sub/:num1/:num2", function(req,res){
+    var number1 = parseFloat(req.params.num1)
+    var number2 = parseFloat(req.params.num2)
+    var sum = number1 - number2
+    res.render("sub", {number:sum})
+});
+app.get("/mult/:num1/:num2", function(req,res){
+    var number1 = parseFloat(req.params.num1)
+    var number2 = parseFloat(req.params.num2)
+    var sum = number1 * number2
+    res.render("mult", {number:sum})
+});
+app.get("/div/:num1/:num2", function(req,res){
+    var number1 = parseFloat(req.params.num1)
+    var number2 = parseFloat(req.params.num2)
+    var sum = number1 + number2
+    res.render("div", {number:sum})
+});
 app.listen(3000);
