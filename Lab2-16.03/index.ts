bd3372f975aec83956a357bbb3cdc3d408813410
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2022-03-21 18:12:47 +0100

    ++

--- Lab2-16.03/index.ts
+++ Lab2-16.03/index.ts
@@ -1,16 +1,64 @@
 import express from 'express'
 import {Request, Response} from 'express'
+import { Note } from './note'
 
 const app = express()
+const notes: Note[] = [
+  {
+    title: "Porsche",
+    content: "Cars",
+    createDate: "21-03-2022",
+    tags:["#car", "#vintage", "#996"],
+    id:1
+  }
+];
 
 app.use(express.json())
-
 app.get('/', function (req: Request, res: Response) {
-  res.send('GET Hello World')
-})
-app.post('/', function (req: Request, res: Response) {
   console.log(req.body) // e.x. req.body.title 
-  res.sendStatus(200).send('POST Hello World')
+  res.status(201).send("Hi. Im simple api for note app");
+})
+
+app.get('/note/:id', function (req: Request, res: Response) {
+  console.log(req.body) // e.x. req.body.title 
+  const note = notes.find(c=>c.id===parseInt(req.params.id));
+  if(!note){
+    res.status(404).send("Can't find")
+  }
+  res.status(200).send(note)
+})
+
+app.post('/note', function (req: Request, res: Response) {
+  console.log("Add me!") 
+  const note = {
+    title: req.body.title,
+    content: req.body.content,
+    createDate: req.body.createDate,
+    tags:req.body.tags,
+    id: notes.length + 1
+  };
+  const title = req.body.title;
+  notes.push(note);
+  res.status(201).send(note);
+})
+
+app.put('/note/:id', function (req: Request, res: Response) {
+  console.log("Read me!")
+  const note = notes.find(c=>c.id===parseInt(req.params.id));
+  if(!note){
+    res.status(404).send("Can't update")
+  };
+  note.title = req.body.title;
+  res.status(203).send(note)
+})
+
+app.delete('/note/:id', function (req: Request, res: Response) {
+  console.log("Read me!")
+  const note = notes.find(c=>c.id===parseInt(req.params.id));
+  if(!note){
+    res.status(404).send("Can't delete")
+  };
+  res.status(204).send(note)
 })
 
 app.listen(3000)
---
+++ Lab2-16.03/note.ts
@@ -0,0 +1,7 @@
+export class Note{
+  title: string;
+  content: string;
+  createDate?: string;
+  tags?: any[];
+  id?: number;
+}
